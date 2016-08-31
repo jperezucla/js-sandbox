8e2866ca383d7f146f41283597c9262193cd8759
Jose Perez <jperezucla@gmail.com>
2016-08-31 15:17:03 -0700

    formatting changes

--- public/js/snap.js
+++ public/js/snap.js
@@ -1,64 +1,86 @@
-/*
- * Copyright (c) 2013 Taye Adeyemi
- * Open source under the MIT License.
- * https://raw.github.com/taye/interact.js/master/LICENSE
- */
-
-(function(interact) {
+(function(interact)
+{
     'use strict';
 
-    var canvas,
-        context,
-        guidesCanvas,
-        guidesContext,
-        width = 600,
-        height = 600,
-        status,
-        prevX = 0,
-        prevY = 0,
-        blue = '#2299ee',
-        lightBlue = '#88ccff',
-        tango = '#ff4400',
-        draggingAnchor = null,
-        balloon_radius = 45,
+    var canvas;
+    var context;
+    var guidesCanvas;
+    var guidesContext;
+    var width = 600;
+    var height = 600;
+    var status;
+    var prevX = 0;
+    var prevY = 0;
+    var blue = '#2299ee';
+    var lightBlue = '#88ccff';
+    var tango = '#ff4400';
+    var draggingAnchor = null;
+    var balloon_radius = 45;
 
-        snapOffset = { x: 0, y: 0 },
+    var snapOffset = {
+        x: 0,
+        y: 0
+    };
 
-        snapGrid = {
-            x: 225,
-            y: 225,
-            range: -1,
-            offset: { x: 75, y: 75 }
+    var snapGrid = {
+        x: 225,
+        y: 225,
+        range: -1,
+        offset: { x: 75, y: 75 }
+    };
+
+    var gridFunc = interact.createSnapGrid(snapGrid);
+    var anchors = [
+        {
+            x: 100,
+            y: 100,
+            range: 200
         },
-        gridFunc = interact.createSnapGrid(snapGrid),
-        anchors = [
-            {x: 100, y: 100, range: 200},
-            {x: 600, y: 400, range: Infinity},
-            {x: 500, y: 150, range: Infinity},
-            {x: 250, y: 250, range: Infinity}
-        ];
+        {
+            x: 600,
+            y: 400,
+            range: Infinity
+        },
+        {
+            x: 500,
+            y: 150,
+            range: Infinity
+        },
+        {
+            x: 250,
+            y: 250,
+            range: Infinity
+        }
+    ];
 
+    function drawGrid(grid, gridOffset, range)
+    {
+        if (!grid.x || !grid.y)
+        {
+            return;
+        }
 
-    function drawGrid (grid, gridOffset, range) {
-        if (!grid.x || !grid.y) { return; }
-
-        var barLength = 16,
-            offset = {
-                x: gridOffset.x + snapOffset.x,
-                y: gridOffset.y + snapOffset.y
-            };
+        var barLength = 16;
+        var offset = {
+            x: gridOffset.x + snapOffset.x,
+            y: gridOffset.y + snapOffset.y
+        };
 
         guidesContext.clearRect(0, 0, width, height);
 
         guidesContext.fillStyle = lightBlue;
 
-        if (range < 0 || range === Infinity) {
+        if (range < 0 || range === Infinity)
+        {
             guidesContext.fillRect(0, 0, width, height);
         }
 
-        for (var i = -(1 + offset.x / grid.x | 0), lenX = width / grid.x + 1; i < lenX; i++) {
-            for (var j = -( 1 + offset.y / grid.y | 0), lenY = height / grid.y + 1; j < lenY; j++) {
-                if (range > 0 && range !== Infinity) {
+        for (var i = -(1 + offset.x / grid.x | 0), lenX = width / grid.x + 1; i < lenX; i++)
+        {
+            for (var j = -( 1 + offset.y / grid.y | 0), lenY = height / grid.y + 1; j < lenY; j++)
+            {
+                if (range > 0 && range !== Infinity)
+                {
                     guidesContext.circle(i * grid.x + offset.x, j * grid.y + offset.y, range, blue).fill();
                 }
 
@@ -75,23 +97,26 @@
         }
     }
 
-    function drawAnchors (anchors, defaultRange) {
+    function drawAnchors(anchors, defaultRange)
+    {
         var barLength = 16;
  
         guidesContext.clearRect(0, 0, width, height);
 
-        if (range < 0 && range !== Infinity) {
+        if (range < 0 && range !== Infinity)
+        {
             guidesContext.fillStyle = lightBlue;
             guidesContext.fillRect(0, 0, width, height);
         }
 
-        for (var i = 0, len = anchors.length; i < len; i++) {
+        for (var i = 0, len = anchors.length; i < len; i++)
+        {
             var anchor = {
-                    x: anchors[i].x + snapOffset.x,
-                    y: anchors[i].y + snapOffset.y,
-                    range: anchors[i].range
-                },
-                range = typeof anchor.range === 'number'? anchor.range: defaultRange;
+                x: anchors[i].x + snapOffset.x,
+                y: anchors[i].y + snapOffset.y,
+                range: anchors[i].range
+            }
+            var range = typeof anchor.range === 'number'? anchor.range: defaultRange;
 
             if (range > 0 && range !== Infinity) {
                 guidesContext.circle(anchor.x, anchor.y, range, blue).fill();
@@ -109,19 +134,23 @@
         }
     }
 
-    function drawSnap (snap) {
+    function drawSnap(snap)
+    {
         context.clearRect(0, 0, width, height);
         guidesContext.clearRect(0, 0, width, height);
 
-        if (status.gridMode.checked) {
+        if (status.gridMode.checked)
+        {
             drawGrid(snapGrid, snapGrid.offset, snapGrid.range);
         }
-        else if (status.anchorMode.checked) {
+        else if (status.anchorMode.checked)
+        {
             drawAnchors(anchors, snap.range);
         }
     }
 
-    function circle (x, y, radius, color) {
+    function circle(x, y, radius, color)
+    {
         this.fillStyle = color || this.fillStyle;
         this.beginPath();
 
@@ -129,14 +158,17 @@
 
         return this;
     }
+
     window.CanvasRenderingContext2D.prototype.circle = circle;
 
-    function dragMove (event) {
+    function dragMove(event)
+    {
         var snap = event.snap;
 
         context.clearRect(0, 0, width, height);
 
-        if (snap && snap.range !== Infinity && typeof snap.x === 'number' && typeof snap.y === 'number') {
+        if (snap && snap.range !== Infinity && typeof snap.x === 'number' && typeof snap.y === 'number')
+        {
             context.circle(snap.x, snap.y, snap.range + 1, 'rgba(102, 225, 117, 0.8)').fill();
         }
 
@@ -146,7 +178,8 @@
         prevY = event.pageY;
     }
 
-    function dragEnd (event) {
+    function dragEnd(event)
+    {
         context.clearRect(0, 0, width, height);
         context.circle(event.pageX, event.pageY, balloon_radius, tango).fill();
 
@@ -154,15 +187,19 @@
         prevY = event.pageY;
     }
 
-    function anchorDragStart (event) {
-        if (event.snap.locked) {
+    function anchorDragStart(event)
+    {
+        if (event.snap.locked)
+        {
             interact(canvas).snap(false);
             draggingAnchor = event.snap.anchors.closest;
         }
     }
 
-    function anchorDragMove (event) {
-        if (draggingAnchor) {
+    function anchorDragMove(event)
+    {
+        if (draggingAnchor)
+        {
             var snap = interact(canvas).snap().drag;
 
             draggingAnchor.x += event.dx;
@@ -172,13 +209,16 @@
         }
     }
 
-    function anchorDragEnd (event) {
+    function anchorDragEnd(event)
+    {
         interact(canvas).draggable({ snap: { enabled: true } });
         draggingAnchor = null;
     }
 
-    function sliderChange (event, valid) {
-        if (!valid) {
+    function sliderChange(event, valid)
+    {
+        if (!valid)
+        {
             return;
         }
 
@@ -189,7 +229,8 @@
         snapGrid.offset.x = Number(status.offsetX.value);
         snapGrid.offset.y = Number(status.offsetY.value);
 
-        if (snapGrid.range < 0) {
+        if (snapGrid.range < 0)
+        {
             snapGrid.range = Infinity;
         }
 
@@ -205,30 +246,11 @@
         snapGrid.offset.x = Number(status.offsetX.value);
         snapGrid.offset.y = Number(status.offsetY.value);
 
-        if (snapGrid.range < 0) {
+        if (snapGrid.range < 0)
+        {
             snapGrid.range = Infinity;
         }
 
-        //drawSnap(interact(canvas).draggable().snap);
-/*        var snap = interact(canvas).draggable().snap;
-
-        if (status.anchorDrag.checked && !status.anchorMode.checked) {
-            status.anchorMode.checked = true;
-        }
-
-        if (status.anchorDrag.checked) {
-            status.anchorMode.disabled = status.offMode.disabled = status.gridMode.disabled = true;
-            status.modes.className += ' disabled';
-
-            interact(canvas)
-                .off('dragstart', dragMove)
-                .off('dragmove', dragMove)
-                .off('dragend', dragEnd)
-                .on('dragstart', anchorDragStart)
-                .on('dragmove', anchorDragMove)
-                .on('dragend', anchorDragEnd);
-        }
-        else {*/
         status.anchorMode.disabled = status.offMode.disabled = status.gridMode.disabled = false;
         status.modes.className = status.modes.className.replace(/ *\<disabled\>/g, '');
 
@@ -239,7 +261,6 @@
             .off('dragstart', anchorDragStart)
             .off('dragmove', anchorDragMove)
             .off('dragend', anchorDragEnd);
-        //}
 
         interact(canvas)
             .draggable({
@@ -255,25 +276,16 @@
                 }
             });
 
-        if (!status.relative.checked) {
+        if (!status.relative.checked)
+        {
             snapOffset.x = snapOffset.y = 0;
         }
 
         drawSnap(interact(canvas).draggable().snap);
     }
 
-/*    function sliderInput (event) {
-        if (event.target.type === 'range' &&
-            (Number(event.target.value) > Number(event.target.max)) ||
-            Number(event.target.value) < Number(event.target.min)) {
-
-            return;
-        }
-
-        sliderChange(event, true);
-    }*/
-
-    interact(document).on('DOMContentLoaded', function () {
+    interact(document).on('DOMContentLoaded', function ()
+    {
         canvas = document.getElementById('drag');
         canvas.width = width;
         canvas.height = height;
@@ -286,8 +298,10 @@
                     restriction: 'self'
                 }
             })
-            .on('move down', function (event) {
-                if ((event.type === 'down' || !event.interaction.pointerIsDown) && status.relative.checked) {
+            .on('move down', function(event)
+            {
+                if ((event.type === 'down' || !event.interaction.pointerIsDown) && status.relative.checked)
+                {
                     var rect = interact.getElementRect(canvas);
 
                     snapOffset.x = event.pageX - rect.left;
@@ -323,13 +337,6 @@
             inertia: document.getElementById('inertia'),
             relative: document.getElementById('relative')
         };
-
-/*        interact(status.sliders)
-            .on('change', sliderChange)
-            .on('input', sliderInput);
-
-        interact(document.getElementById('modes'))
-            .on('change', modeChange);*/
 
         sliderChange(null, true);
         init();
